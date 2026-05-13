6cc08c73895b21dfdbe47a8884cf3cdd7cfc35b8
1m1 <imi@1m1.io>
2026-05-12 21:44:39 -0500

    .

---
+++ src/TOGZMQClient.jl
@@ -0,0 +1,22 @@
+module TOGZMQClient
+
+export togtime, create, observe
+
+using TOGZMQAPIClient
+
+start = TOGZMQAPIClient.start
+call = TOGZMQAPIClient.call
+
+# togtime() = TOGZMQAPIClient.call(TOGZMQAPIClient.FUNCTIONS[:time])
+# create(x) = TOGZMQAPIClient.call(TOGZMQAPIClient.FUNCTIONS[:create], x)
+# observe(x) = TOGZMQAPIClient.call(TOGZMQAPIClient.FUNCTIONS[:observe], x)
+
+togtime() = TOGZMQAPIClient.call(:time)
+create(x) = TOGZMQAPIClient.call(:create, x)
+observe(x) = TOGZMQAPIClient.call(:observe, x)
+
+function start(socketlocation)
+    TOGZMQAPIClient.start(socketlocation)
+end
+
+end
