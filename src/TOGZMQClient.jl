bf14d825ecb6932022f977885744e7941de95830
1m1 <imi@1m1.io>
2026-06-03 17:27:19 -0500

    2.0.0

--- src/TOGZMQClient.jl
+++ src/TOGZMQClient.jl
@@ -7,16 +7,8 @@ using TOGZMQAPIClient
 awaken = TOGZMQAPIClient.awaken
 call = TOGZMQAPIClient.call
 
-# togtime() = TOGZMQAPIClient.call(TOGZMQAPIClient.FUNCTIONS[:time])
-# create(x) = TOGZMQAPIClient.call(TOGZMQAPIClient.FUNCTIONS[:create], x)
-# observe(x) = TOGZMQAPIClient.call(TOGZMQAPIClient.FUNCTIONS[:observe], x)
-
 togtime() = TOGZMQAPIClient.call(:time)
 create(x) = TOGZMQAPIClient.call(:create, x)
 observe(x) = TOGZMQAPIClient.call(:observe, x)
-
-# function start(socketlocation)
-#     TOGZMQAPIClient.start(socketlocation)
-# end
 
 end
