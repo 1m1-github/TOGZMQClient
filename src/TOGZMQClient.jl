08e1a16a9cfc6a4c519bdbeff3993c62f5d44c43
1m1 <imi@1m1.io>
2026-06-17 15:17:34 -0500

    3.0.0

--- src/TOGZMQClient.jl
+++ src/TOGZMQClient.jl
@@ -8,7 +8,7 @@ awaken = TOGZMQAPIClient.awaken
 call = TOGZMQAPIClient.call
 
 togtime() = TOGZMQAPIClient.call(:time)
-create(x) = TOGZMQAPIClient.call(:create, x)
-observe(x) = TOGZMQAPIClient.call(:observe, x)
+create(x...) = TOGZMQAPIClient.call(:create, x...)
+observe(x...) = TOGZMQAPIClient.call(:observe, x...)
 
 end
